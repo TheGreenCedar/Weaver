992478f311ebaef666ad1b9068a1f434153e7b72
Albert <albertnaggear@gmail.com>
2020-04-13 09:07:22 -0400

    Add project files.

---
+++ Weaver/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Weaver.Data.Models;
+
+namespace Weaver.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
